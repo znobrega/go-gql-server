3fa3bdec84070db710a31e6bad16e24f1f141c4b
Carlos Nobrega <nobregacarlosjr@gmail.com>
2020-06-24 11:49:43 -0300

    feat: add model on models package

---
+++ internal/orm/models/videos.go
@@ -0,0 +1,9 @@
+package models
+
+// User defines a user for the app
+type Video struct {
+	BaseModelSoftDelete         // We don't to actually delete the users, maybe audit if we want to hard delete them? or wait x days to purge from the table, also
+	Title               string  `gorm:"not null;unique_index:idx_email"`
+	VideoID             *string // External user ID
+	Url                 *string
+}
